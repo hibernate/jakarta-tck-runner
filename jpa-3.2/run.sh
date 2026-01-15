20f8ff3abef13ca88f36035d179ce81764e95cdf
marko-bekhta <marko.prykladna@gmail.com>
2026-01-15 16:59:29 +0100

    Override JUnit version for Jakarta Persistence 3.2 TCK and more modern Hibernate ORM versions

--- jpa-3.2/run.sh
+++ jpa-3.2/run.sh
@@ -9,38 +9,52 @@ else
 	GF_VERSION=8.0.0-M5
 fi
 
+EXTRA_BUILD_ARGS=
+
+HIBERNATE_ORM_MAJOR=$(echo "$HIBERNATE_VERSION" | cut -d. -f1)
+HIBERNATE_ORM_MINOR=$(echo "$HIBERNATE_VERSION" | cut -d. -f2)
+
+if (( HIBERNATE_ORM_MAJOR > 7 || HIBERNATE_ORM_MINOR != 0 )); then
+    EXTRA_BUILD_ARGS="-Djunit.jupiter.version=5.13.4"
+fi
+
 set -x
 if [ "$RDBMS" == 'mysql' ]; then
   mvn -e -f ${TCK_HOME}/persistence-tck/bin/pom.xml -P "hibernate,mysql" verify "-Dhibernate.version=${HIBERNATE_VERSION}" \
     "-Dglassfish.container.version=${GF_VERSION}" \
     "-Djakarta.persistence.jdbc.user=hibernate_orm_test" \
     "-Djakarta.persistence.jdbc.password=hibernate_orm_test" \
-    "-Djakarta.persistence.jdbc.url=jdbc:mysql://${DB_HOST:-localhost}/hibernate_orm_test?allowPublicKeyRetrieval=true"
+    "-Djakarta.persistence.jdbc.url=jdbc:mysql://${DB_HOST:-localhost}/hibernate_orm_test?allowPublicKeyRetrieval=true" \
+    "$EXTRA_BUILD_ARGS"
 elif [ "$RDBMS" == 'postgresql' ]; then
   mvn -e -f ${TCK_HOME}/persistence-tck/bin/pom.xml -P "hibernate,postgresql" verify "-Dhibernate.version=${HIBERNATE_VERSION}" \
     "-Dglassfish.container.version=${GF_VERSION}" \
     "-Djakarta.persistence.jdbc.user=hibernate_orm_test" \
     "-Djakarta.persistence.jdbc.password=hibernate_orm_test" \
     "-Djakarta.persistence.jdbc.url=jdbc:postgresql://${DB_HOST:-localhost}/hibernate_orm_test?preparedStatementCacheQueries=0&escapeSyntaxCallMode=callIfNoReturn" \
-    "-Dpostgresql.jdbc.version=42.7.4"
+    "-Dpostgresql.jdbc.version=42.7.4" \
+    "$EXTRA_BUILD_ARGS"
 elif [ "$RDBMS" == 'db2' ]; then
   mvn -e -f ${TCK_HOME}/persistence-tck/bin/pom.xml -P "hibernate,db2" verify "-Dhibernate.version=${HIBERNATE_VERSION}" \
     "-Dglassfish.container.version=${GF_VERSION}" \
     "-Djakarta.persistence.jdbc.user=orm_test" \
     "-Djakarta.persistence.jdbc.password=orm_test" \
-    "-Djakarta.persistence.jdbc.url=jdbc:db2://${DB_HOST:-localhost}:50000/orm_test"
+    "-Djakarta.persistence.jdbc.url=jdbc:db2://${DB_HOST:-localhost}:50000/orm_test"\
+    "$EXTRA_BUILD_ARGS"
 elif [ "$RDBMS" == 'oracle' ]; then
   mvn -e -f ${TCK_HOME}/persistence-tck/bin/pom.xml -P "hibernate,oracle" verify "-Dhibernate.version=${HIBERNATE_VERSION}" \
     "-Dglassfish.container.version=${GF_VERSION}" \
     "-Djakarta.persistence.jdbc.user=hibernate_orm_test" \
     "-Djakarta.persistence.jdbc.password=hibernate_orm_test" \
-    "-Djakarta.persistence.jdbc.url=jdbc:oracle:thin:@${DB_HOST:-localhost}:1521/freepdb1"
+    "-Djakarta.persistence.jdbc.url=jdbc:oracle:thin:@${DB_HOST:-localhost}:1521/freepdb1" \
+    "$EXTRA_BUILD_ARGS"
 elif [ "$RDBMS" == 'mssql' ]; then
   mvn -e -f ${TCK_HOME}/persistence-tck/bin/pom.xml -P "hibernate,mssqlserver" verify "-Dhibernate.version=${HIBERNATE_VERSION}" \
     "-Dglassfish.container.version=${GF_VERSION}" \
     "-Djakarta.persistence.jdbc.user=sa" \
     "-Djakarta.persistence.jdbc.password=Hibernate_orm_test" \
-    "-Djakarta.persistence.jdbc.url=jdbc:sqlserver://${DB_HOST:-localhost};databaseName=hibernate_orm_test;sendTimeAsDatetime=false;trustServerCertificate=true"
+    "-Djakarta.persistence.jdbc.url=jdbc:sqlserver://${DB_HOST:-localhost};databaseName=hibernate_orm_test;sendTimeAsDatetime=false;trustServerCertificate=true" \
+    "$EXTRA_BUILD_ARGS"
 elif [ "$RDBMS" == 'sybase' ]; then
   mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file -Dfile=${TCK_HOME}/jconn42.jar -DgroupId=com.sybase -DartifactId=jdbc42 -Dversion=1.0.0 -Dpackaging=jar
   mvn -e -f ${TCK_HOME}/persistence-tck/bin/pom.xml -P "hibernate,sybase" verify "-Dhibernate.version=${HIBERNATE_VERSION}" \
@@ -48,14 +62,16 @@ elif [ "$RDBMS" == 'sybase' ]; then
     "-Djakarta.persistence.jdbc.user=hibernate_orm_test" \
     "-Djakarta.persistence.jdbc.password=hibernate_orm_test" \
     "-Djakarta.persistence.jdbc.url=jdbc:sybase:Tds:${DB_HOST:-localhost}:9000/hibernate_orm_test" \
-	"-Djakarta.persistence.jdbc.driver=com.sybase.jdbc4.jdbc.SybDriver" \
-	"-Djpa.provider.implementation.specific.additional.properties=:connection.init_sql=set ansinull on set quoted_identifier on"
+    "-Djakarta.persistence.jdbc.driver=com.sybase.jdbc4.jdbc.SybDriver" \
+    "-Djpa.provider.implementation.specific.additional.properties=:connection.init_sql=set ansinull on set quoted_identifier on" \
+    "$EXTRA_BUILD_ARGS"
 elif [ "$RDBMS" == 'derby' ] || [ -z "$RDBMS" ]; then
   mvn -e -f ${TCK_HOME}/persistence-tck/bin/pom.xml -P "hibernate,derby" verify "-Dhibernate.version=${HIBERNATE_VERSION}" \
     "-Dglassfish.container.version=${GF_VERSION}" \
     "-Djakarta.persistence.jdbc.user=hibernate_orm_test" \
     "-Djakarta.persistence.jdbc.password=hibernate_orm_test" \
-    "-Djakarta.persistence.jdbc.url=jdbc:derby://${DB_HOST:-localhost}:1527/derbyDB;create=true"
+    "-Djakarta.persistence.jdbc.url=jdbc:derby://${DB_HOST:-localhost}:1527/derbyDB;create=true" \
+    "$EXTRA_BUILD_ARGS"
 else
   echo "Unsupported RDBMS: ${RDBMS}"
   exit 1
